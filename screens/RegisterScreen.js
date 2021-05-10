89a808dd0b68b85b624af7474080bac47552d87a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 15:20:56 +0300

    Rokas

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,43 @@
-import React from "react";
-import {StyleSheet, Text, View} from "react-native"
+import React, {useState} from "react";
+import {KeyboardAvoidingView, StyleSheet, Text, View} from "react-native"
+import {StatusBar} from "expo-status-bar";
+import {Input} from "react-native-elements";
 
 const RegisterScreen = ({ navigation }) => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
+    const register = () => {
+
+    }
+
     return (
-        <View>
-            <Text> I am register screen</Text>
-        </View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
+            <Text h3 style={{ marginBottom: 50 }}>
+                Create a Signal Account
+            </Text>
+
+            <View style={styles.inputContainer}>
+                <Input placeholder="Full name" autoFocus type="text" value={name} onChangeText={(text) => setName(text)}/>
+                <Input placeholder="Email" type="email" value={email} onChangeText={(text) => setEmail(text)}/>
+                <Input placeholder="Password" type="password" secureTextEntry value={password} onChangeText={(text) => setPassword(text)}/>
+                <Input placeholder="Profile Picture URL (optional)" type="text" value={imageUrl} onChangeText={(text) => setImageUrl(text)} onSubmitEditing={register}/>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    inputContainer: {
+
+    },
+})
