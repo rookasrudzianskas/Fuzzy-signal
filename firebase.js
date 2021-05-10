84a5a4e84f2c925076c294b92436749940da7cf0
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 14:02:54 +0300

    Rokas

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAsXrb3bLhTJgxgwD76fdYDb-7bExcQc10",
+    authDomain: "chat-app-5bfa5.firebaseapp.com",
+    projectId: "chat-app-5bfa5",
+    storageBucket: "chat-app-5bfa5.appspot.com",
+    messagingSenderId: "761077059554",
+    appId: "1:761077059554:web:96767261cb4092811e33bb",
+    measurementId: "G-LHKKPVQC8H"
+};s
