1a25bc3f2b902793e93962a20561fe60fbb5a936
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 15:00:23 +0300

    Rokas

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {KeyboardAvoidingView, StyleSheet, Text, View} from "react-native";
 import {Button, Input, Image} from "react-native-elements";
 import {StatusBar} from "expo-status-bar";
 
@@ -9,8 +9,13 @@ const LoginScreen = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+// sign in function with on PRess from the button
+    const signIn = () => {
+
+    }
+
     return (
-        <View>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
             {/* all that battery icons and so on*/}
             <StatusBar style="light" />
             {/* image file*/}
@@ -24,15 +29,23 @@ const LoginScreen = () => {
                 <Input placeholder="Password" secureTextEntry autoFocus type="password" value={password} onChangeText={(text) => setPassword(text)} />
             </View>
             {/* in order to style anything, you have to wrap it in the containerStyle and then add styles.something*/}
-            <Button containerStyle={styles.button} title="Login" />
+            <Button containerStyle={styles.button} onPress={signIn} title="Login" />
             <Button containerStyle={styles.button} type="outline" title="Register" />
-        </View>
+            <View style={{ height: 100 }} />
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "white",
+    },
     inputContainer: {
 
     },
