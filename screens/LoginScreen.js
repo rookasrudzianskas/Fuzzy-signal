774dad780c770d3e91722d4d432bf6545e4a4a88
Rokas <rookas.rudzenskas@gmail.com>
2021-05-10 14:41:39 +0300

    Roaks

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,12 +7,20 @@ import {StatusBar} from "expo-status-bar";
 const LoginScreen = () => {
     return (
         <View>
+            {/* all that battery icons and so on*/}
             <StatusBar style="light" />
-            <Text>I am the login</Text>
+            {/* image file*/}
+            <Image source={{
+                uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Signal-Logo.svg/1200px-Signal-Logo.svg.png",
+            }}
+            style={{width: 200, height: 200}}
+            />
         </View>
     )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+});
