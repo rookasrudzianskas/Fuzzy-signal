d48d53dd65d43a94182a396c84348985b309d9a3
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 18:01:39 +0300

    Rokas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useLayoutEffect} from "react";
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from "react-native"
 import CustomListItem from "../components/CustomListItem";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Rookas Chat App",
+            headerStyle: { backgroundColor: "#fff" },
+            headerTitleStyle: { color: "black" },
+            headerTintColor: "black"
+        });
+    }, []);
+
     return (
         <SafeAreaView>
             <ScrollView>
