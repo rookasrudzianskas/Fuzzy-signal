4232e9626cb4871f17a54f573e2113712782759f
Rokas <rookas.rudzenskas@gmail.com>
2021-05-17 21:27:09 +0300

    Rokas

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useLayoutEffect, useState} from "react";
 import {StyleSheet, Text, View} from "react-native"
+import { Button, Input} from "react-native-elements/dist/input/Input";
+import {Icon} from "react-native-elements";
+import {AntDesign, Entypo, Foundation, SimpleLineIcons} from "@expo/vector-icons";
 
-const AddChatScreen = () => {
+const AddChatScreen = ({ navigation }) => {
+
+    const [input, setInput] = useState("");
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Add a new chat",
+            headerBackTitle: "Chats",
+        })
+    }, []);
     return (
-        <View>
-            <Text>AddChatScreen</Text>
+        <View style={styles.container}>
+            <Input placeholder="Enter a chat name" value={input} onChangeText={text => setInput(text)} leftIcon={<Icon name="wechat" type="antdesign" size={24} color="black"/>}/>
         </View>
     )
 }
 
 export default AddChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+   container: {
+
+   }
+});
