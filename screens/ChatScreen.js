87e8d7f93de6fdaf7cfb4a6b2edf0122d9d315a1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 17:06:58 +0300

    Rokas

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,7 +1,17 @@
 import React, {useLayoutEffect} from "react";
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native"
+import {
+    Image,
+    KeyboardAvoidingView, Platform,
+    SafeAreaView, ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text, TextInput,
+    TouchableOpacity,
+    View
+} from "react-native"
 import {Avatar} from "react-native-elements";
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
+import {ImageBackground} from "react-native";
 
 const ChatScreen = ({ navigation, route }) => {
 
@@ -21,18 +31,59 @@ const ChatScreen = ({ navigation, route }) => {
                 <TouchableOpacity style={{ marginLeft: 10 }} onPress={navigation.goBack}>
                     <AntDesign name="arrowleft" size={24} color="white" />
                 </TouchableOpacity>
-            )
+            ),
+
+            headerRight: () => (
+              <View style={{ flexDirection: "row", justifyContent: "space-between", width: 80, marginRight: 20, }}>
+                  <TouchableOpacity>
+                      <FontAwesome name="video-camera" size={24} color="white" />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity>
+                      <Ionicons name="call" size={24} color="white" />
+                  </TouchableOpacity>
+              </View>
+            ),
         });
     }, [navigation]);
 
-
+    // style={{ flex: 1, backgroundImage: "https://theabbie.github.io/blog/assets/official-whatsapp-background-image.jpg" }}
     return (
-        <View>
-            <Text>{route.params.chatName}</Text>
-        </View>
+        <SafeAreaView  >
+            {/*<Image*/}
+            {/*    source={{uri: 'https://theabbie.github.io/blog/assets/official-whatsapp-background-image.jpg'}}*/}
+            {/*    style={{*/}
+            {/*        flex: 1,*/}
+            {/*        width: null,*/}
+            {/*        height: null,*/}
+            {/*        resizeMode: 'stretch',*/}
+            {/*    }}*/}
+            {/*>*/}
+            <StatusBar style="auto" />
+            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.container} keyboardVerticalOffset={90}>
+                <>
+                    <ScrollView>
+                    {/*    chat goes in here    */}
+
+                    </ScrollView>
+                    <View style={styles.footer}>
+                        <TextInput placeholder="Rookas Message"/>
+                    </View>
+                </>
+            </KeyboardAvoidingView>
+            {/*</Image>*/}
+        </SafeAreaView>
     )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+
+    },
+
+    footer: {
+
+    },
+})
