c85f85a4eefd383747ae538c32a3835dac84b582
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 14:44:16 +0300

    Roaks

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {StyleSheet, Text, View} from "react-native"
 import {ListItem, Avatar} from "react-native-elements";
+import db from "../firebase";
 
 const CustomListItem = ({ id, chatName, enterChat, timestamp }) => {
+
+    const [chatMessages, setChatMessages] = useState([]);
+    console.log("Chat messages >>>>", chatMessages.length)
+    useEffect(() => {
+        const unsubscribe = db.collection("chats").doc(id).collection("messages").orderBy('timestamp', "desc").onSnapshot(snapshot => {
+            setChatMessages(snapshot.docs.map(doc => doc.data()))
+        });
+
+        return unsubscribe;
+    })
     return (
         // this calls a function enterChat
         <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
             <Avatar
                 rounded
-                source={{uri: "https://www.vhv.rs/dpng/d/505-5058560_person-placeholder-image-free-hd-png-download.png"}}
+                source={{uri: chatMessages?.[0]?.photoURL || "https://www.vhv.rs/dpng/d/505-5058560_person-placeholder-image-free-hd-png-download.png"}}
             />
 
             <ListItem.Content>
